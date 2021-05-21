976a379398868bd195bdb2367a39ef278b547857
王俊淵 <wangjunyuan@wangjunyuandeMacBook-Pro.local>
2021-05-21 17:38:43 +0800

    set meta title

--- src/app/pages/home/home-item/home-item.component.ts
+++ src/app/pages/home/home-item/home-item.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-item',
   templateUrl: './home-item.component.html',
-  styleUrls: ['./home-item.component.scss']
+  styleUrls: ['./home-item.component.scss'],
 })
 export class HomeItemComponent implements OnInit {
+  constructor(private metaTagService: Meta, private titleService: Title) {
+    this.metaTagService.addTags([
+      {
+        name: 'keywords test',
+        content: 'Angular SEO Integration, Music CRUD, Angular Universal',
+      },
+    ]);
 
-  constructor() { }
-
-  ngOnInit(): void {
+    this.titleService.setTitle('DKSH test');
   }
 
+  ngOnInit(): void {}
 }
