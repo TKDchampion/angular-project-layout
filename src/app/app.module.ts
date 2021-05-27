118f0e58672b9de00d0e16e5b845308342c98dc6
王俊淵 <wangjunyuan@wangjunyuandeMacBook-Pro.local>
2021-05-27 11:35:15 +0800

    join to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpDefaultOptions, JWTOptions } from './core/model/option';
+
+export class DKSHitHttpDefaultOptions extends HttpDefaultOptions {
+  baseApiURL = environment.baseApiUrl; // input api base url.
+}
+
+export class DKSHJWTOptions extends JWTOptions {
+  key = 'user';
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule.withServerTransition({ appId: 'serverApp' }), AppRoutingModule],
-  providers: [],
+  providers: [
+    { provide: HttpDefaultOptions, useClass: DKSHitHttpDefaultOptions },
+    { provide: JWTOptions, useClass: DKSHJWTOptions },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
