50cd5e17c4f976c6fa381fd54476e583d1af3a8c
王俊淵 <wangjunyuan@wangjunyuandeMacBook-Pro.local>
2021-05-28 17:42:06 +0800

    can't use ssr

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,36 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-// import { GlobalStateConstant } from 'src/app/core/constant/global-state.constant';
-// import { GlobalStateService } from 'src/app/core/services/global-state.service';
+import { Component } from '@angular/core';
+import { GlobalStateConstant } from 'src/app/core/constant/global-state.constant';
+import { GlobalStateService } from 'src/app/core/services/global-state.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  // constructor(private state: GlobalStateService) {}
-
-  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
-  ngOnInit(): void {
-    // this.Editor.create(document.querySelector('#editor'), {
-    //   cloudServices: {
-    //     tokenUrl: 'https://example.com/cs-token-endpoint',
-    //     uploadUrl: 'https://your-organization-id.cke-cs.com/easyimage/upload/',
-    //   },
-    // })
-    //   .then((resp: unknown) => {
-    //     console.log(resp);
-    //   })
-    //   .catch((error: unknown) => {
-    //     console.log(error);
-    //   });
+export class HomeComponent {
+  constructor(private state: GlobalStateService) {}
+  click(): void {
+    this.state.notifyDataChanged(GlobalStateConstant.test1.cmd, Math.random());
   }
-
-  // click(): void {
-  //   this.state.notifyDataChanged(GlobalStateConstant.test1.cmd, Math.random());
-  // }
-
-  // click2(): void {
-  //   this.state.notifyDataChanged(GlobalStateConstant.test2.cmd, Math.random());
-  // }
+  click2(): void {
+    this.state.notifyDataChanged(GlobalStateConstant.test2.cmd, Math.random());
+  }
 }
