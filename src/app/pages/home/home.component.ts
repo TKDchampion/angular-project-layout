5472a3738e1f59a008318740817071a8ee010466
TKDchampion <as1219123@yahoo.com.tw>
2021-06-03 16:22:17 +0800

    player

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AfterViewInit, Component, ViewChildren } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { open, close } from 'src/app/core/store/flag/flag.actions';
 import { setValue } from 'src/app/core/store/setValue/set-value.actions';
+import Player from '@vimeo/player';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   flag$: Observable<boolean>;
-  // getValue$: Observable<{ value: string }>;
   getdata: Record<string, unknown> | undefined;
 
   constructor(private store: Store<{ flag: boolean }>, private storeValue: Store<{ setValue: { value: string } }>) {
@@ -19,6 +21,16 @@ export class HomeComponent {
     storeValue.select('setValue').subscribe((resp) => {
       console.log(resp);
       this.getdata = resp;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    const player = new Player('handstick', {
+      id: 19231868,
+      width: 640,
+    });
+    player.on('play', function () {
+      console.log('played the video!');
     });
   }
 
