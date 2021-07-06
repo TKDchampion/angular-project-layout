fcda4f39ca1774f2982cdfbbc35e53b9fdae7088
TKDchampion <as1219123@yahoo.com.tw>
2021-07-06 14:50:52 +0800

    changed bootstrap

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { StoreModule } from '@ngrx/store';
-import { CommonToolModule } from '../common/common-tool.module';
+import { CommonToolModule } from '../common-tool/common-tool.module';
 @NgModule({
   declarations: [PagesComponent],
   imports: [CommonModule, PagesRoutingModule, StoreModule.forRoot({}), CommonToolModule],
