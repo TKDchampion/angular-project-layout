151d51d04755fda727e5233a2e1c8ea18e8eca74
TKDchampion <as1219123@yahoo.com.tw>
2021-07-16 16:41:10 +0800

    fix flex

--- src/app/common-tool/banner/banner.component.ts
+++ src/app/common-tool/banner/banner.component.ts
@@ -4,14 +4,12 @@ import { EveService } from 'src/app/services/env.service';
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+  styleUrls: ['./banner.component.scss'],
 })
 export class BannerComponent implements OnInit {
   // identity = this.envService.getEnv('identity');
   identity = 2;
   constructor(private envService: EveService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
