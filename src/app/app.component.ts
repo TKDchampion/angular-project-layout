14a4be0025fc931af96c83bff7d35e667468e757
TKDchampion <as1219123@yahoo.com.tw>
2021-08-03 15:06:47 +0800

    environment deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from 'src/environments/environment';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor() {
+    console.log(environment);
+  }
   title = 'dksh';
 }
--- src/app/pages/member-edit/member-edit.component.ts
+++ src/app/pages/member-edit/member-edit.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MenuInfo } from './member-edit.model';
 
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
-  styleUrls: ['./member-edit.component.scss']
+  styleUrls: ['./member-edit.component.scss'],
 })
-export class MemberEditComponent implements OnInit {
+export class MemberEditComponent {
+  menulist: MenuInfo[] = [
+    { name: '會員資料', active: true },
+    { name: '瀏覽紀錄', active: false },
+    { name: '我的收藏', active: false },
+  ];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  selectMenu(item: MenuInfo): void {
+    this.menulist.forEach((i) => (i.active = false));
+    item.active = true;
   }
-
 }
