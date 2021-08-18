0d86bc0c736773716fe712baa7c19fcaec17afb8
TKDchampion <as1219123@yahoo.com.tw>
2021-08-18 15:39:15 +0800

    add feature article detail and judgement

--- src/app/pages/feature-article/feature-article.module.ts
+++ src/app/pages/feature-article/feature-article.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { FeatureArticleRoutingModule } from './feature-article-routing.module';
 import { FeatureArticleComponent } from './feature-article.component';
 import { CommonToolModule } from 'src/app/common-tool/common-tool.module';
+import { FeatureArticleDetailpageComponent } from './feature-article-detailpage/feature-article-detailpage.component';
 
 @NgModule({
-  declarations: [FeatureArticleComponent],
+  declarations: [FeatureArticleComponent, FeatureArticleDetailpageComponent],
   imports: [CommonModule, FeatureArticleRoutingModule, CommonToolModule],
 })
 export class FeatureArticleModule {}
