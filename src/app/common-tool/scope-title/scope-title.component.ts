317c24842f36565fd04e37cd71507d9558624b3a
Jeff <st47146621@gmail.com>
2021-08-19 09:52:58 +0800

    add input url on scope title

--- src/app/common-tool/scope-title/scope-title.component.ts
+++ src/app/common-tool/scope-title/scope-title.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { EveService } from 'src/app/services/env.service';
 
 @Component({
@@ -11,13 +12,20 @@ export class ScopeTitleComponent {
   @Input() showMoreBtn = true;
   @Input() showTag = true;
   @Input() customClass: string | undefined;
+  @Input() url: string | undefined;
   tag: string | undefined;
   tags = [
     { id: 1, text: '登入後即可瀏覽內容' },
     { id: 2, text: '驗證後即可解鎖專業性醫學內容' },
   ];
   identity = this.envService.getEnv('identity');
-  constructor(private envService: EveService) {
+  constructor(private envService: EveService, private router: Router) {
     this.tag = this.tags.find((i) => i.id === this.identity)?.text;
+  }
+
+  linkToPage() {
+    if (this.url) {
+      this.router.navigate([this.url]);
+    }
   }
 }
