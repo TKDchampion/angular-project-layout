daf19b6596fb31c7448cad2e4d96457d0f256e85
TKDchampion <as1219123@yahoo.com.tw>
2021-08-20 14:40:28 +0800

    finished article detail but swiper common tool not even close

---
+++ src/app/common-tool/swiper/swiper.model.ts
@@ -0,0 +1,49 @@
+import { NewArticleModel } from '../new-article/new-article.modal';
+
+export const NewArticle: NewArticleModel[] = [
+  {
+    id: '1',
+    articleImg: '../../../assets/img/fakeImge/new_article_1.png',
+    articleTitle: '加護病房靜脈同時給予多種藥品之型態與風險',
+    articleSubtitle: '對某醫學中心11個加護病房的護理師問券調查，以了解同時竟賣給多種藥的型態與風險。',
+    author: '黃織芬',
+    authorImg: '../../../assets/img/fakeImge/new_article_headshot_1.png',
+    viewer: '2021/05/11．1,567',
+  },
+  {
+    id: '2',
+    articleImg: '../../../assets/img/fakeImge/new_article_2.png',
+    articleTitle: '甲狀腺癌術後病人採低碘飲食持續時間對尿碘濃度之影響',
+    articleSubtitle: '本研究目的為探討甲狀腺癌術後病人採低碘飲食的持續時間對尿碘濃度之影響。',
+    author: '黃織芬',
+    authorImg: '../../../assets/img/fakeImge/header_fake_headshot.png',
+    viewer: '2021/05/11．1,567',
+  },
+  {
+    id: '3',
+    articleImg: '../../../assets/img/fakeImge/new_article_1.png',
+    articleTitle: '加護病房靜脈同時給予多種藥品之型態與風險',
+    articleSubtitle: '對某醫學中心11個加護病房的護理師問券調查，以了解同時竟賣給多種藥的型態與風險。',
+    author: '黃織芬',
+    authorImg: '../../../assets/img/fakeImge/new_article_headshot_1.png',
+    viewer: '2021/05/11．1,567',
+  },
+  {
+    id: '4',
+    articleImg: '../../../assets/img/fakeImge/new_article_2.png',
+    articleTitle: '甲狀腺癌術後病人採低碘飲食持續時間對尿碘濃度之影響',
+    articleSubtitle: '本研究目的為探討甲狀腺癌術後病人採低碘飲食的持續時間對尿碘濃度之影響。',
+    author: '黃織芬',
+    authorImg: '../../../assets/img/fakeImge/header_fake_headshot.png',
+    viewer: '2021/05/11．1,567',
+  },
+  {
+    id: '5',
+    articleImg: '../../../assets/img/fakeImge/new_article_1.png',
+    articleTitle: '加護病房靜脈同時給予多種藥品之型態與風險',
+    articleSubtitle: '對某醫學中心11個加護病房的護理師問券調查，以了解同時竟賣給多種藥的型態與風險。',
+    author: '黃織芬',
+    authorImg: '../../../assets/img/fakeImge/new_article_headshot_1.png',
+    viewer: '2021/05/11．1,567',
+  },
+];
