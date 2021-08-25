a494087b04e796b3b7289f8a59285cba91bbde32
Jeff <st47146621@gmail.com>
2021-08-25 09:39:12 +0800

    add login popup if click video or article

--- src/app/common-tool/new-video/new-video.component.ts
+++ src/app/common-tool/new-video/new-video.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { EveService } from 'src/app/services/env.service';
+import { LoginModalComponent } from '../popup/login-modal/login-modal.component';
 import { NewVideoItemInfoNewArticleModel } from './new-video.model';
 
 @Component({
@@ -8,4 +12,24 @@ import { NewVideoItemInfoNewArticleModel } from './new-video.model';
 })
 export class NewVideoComponent {
   @Input() newVideoItem!: NewVideoItemInfoNewArticleModel;
+  modalRef!: BsModalRef;
+  identity = this.envService.getEnv('identity');
+  constructor(private envService: EveService, private modalService: BsModalService, private router: Router) { }
+
+  clickVideo(newVideoItem: NewVideoItemInfoNewArticleModel) {
+    if (this.identity === 1) {
+      this.login();
+    } else {
+      this.router.navigate([`/pages/feature-video`, newVideoItem.type ,newVideoItem.id]);
+    }
+  }
+
+  login() {
+    this.modalRef = this.modalService.show(LoginModalComponent, {
+      class: 'modal-dialog-centered modal_max_width',
+      ignoreBackdropClick: true,
+      keyboard: false,
+    });
+    this.modalRef.content.modalRef = this.modalRef;
+  }
 }
